9d7fd45bfffe58f0091d5aa394503d60d5068d44
Ronni Louie <ronnilouie@gmail.com>
2018-12-20 16:23:25 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,20 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"]
+var cardsInPlay = [];
+var cardOne = cards[0];
 
-console.log ("User flipped " + cardOne);
-console.log ("User flipped " + cardFour);
+cardsInPlay.push(cardOne);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay [1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
+
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
