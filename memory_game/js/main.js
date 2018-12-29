980142d888e1af91d99b8dff3733de55cd0c817c
Ronni Louie <ronnilouie@gmail.com>
2018-12-28 19:15:59 -0800

    edited game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -20,25 +20,55 @@ var cards = [
 		cardImage: "images/king-of-diamonds.png"
 	}
 ];
+
 var cardsInPlay = [];
 
 var checkForMatch = function () {
+	//var scoreTotal = 1;
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
+		/*
+		//adds scoreTotal to paragraph
+		var score = document.getElementById("score-listing");
+		score.textContent = "Score: " + scoreTotal;
+		//inrements score with each match
+		scoreTotal += 1;
+		*/
 	} else {
 		alert("Sorry, try again.");
 	}
 }
 
-var flipCard = function (cardId) {
+var flipCard = function () {
+	//gets card's position in array
+	var cardId = this.getAttribute("data-id");
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute("src", cards[cardId].cardImage);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	};
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement("img");
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	};
+};
+
+createBoard();
+
+var reset = function () {
+	cardsInPlay = [];
+	document.getElementById("game-board").innerHTML = "";
+	createBoard();
+};
+
+document.querySelector("button").addEventListener("click", reset);
+
